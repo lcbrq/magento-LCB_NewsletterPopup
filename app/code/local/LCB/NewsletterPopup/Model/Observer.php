0ae7b710c930d1feb5df6e3130a54d243b0c4277
Silpion <tomasz@silpion.com.pl>
2016-08-22 23:02:19 +0200

    Adds newsletter subscription observer for one year cookie set

---
+++ app/code/local/LCB/NewsletterPopup/Model/Observer.php
@@ -0,0 +1,19 @@
+<?php
+
+class LCB_NewsletterPopup_Model_Observer {
+
+    public function subscribedToNewsletter(Varien_Event_Observer $observer)
+    {
+        $event = $observer->getEvent();
+        $subscriber = $event->getDataObject();
+        $data = $subscriber->getData();
+        $statusChange = $subscriber->getIsStatusChanged();
+
+        if ($data['subscriber_status'] == 1 && $statusChange == true) {
+            Mage::getModel('core/cookie')->set('newsletterPopupShown', 'true', 31536000);
+        }
+
+        return $observer;
+    }
+
+}
